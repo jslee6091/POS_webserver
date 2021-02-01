a81a94e6e8bd16070efd22d0df20f711c4212a67
jslee6091 <jslee6091@naver.com>
2021-02-01 15:48:57 +0900

    heroku project

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 // http://127.0.0.1:3000/
 app.get('/', (req, res) => res.send('Hello nodejs jaesung'));
+app.get('/welcome', (req, res)=> res.send('hello welcome to the Nodejs service'));
 app.listen(port, () => console.log('example app listening on port 3000'))
