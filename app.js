27108c0885f3f220a1a3aa81dc595986ebcbbe04
jslee6091 <jslee6091@naver.com>
2021-02-01 15:02:04 +0900

    first heroku

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+// http://127.0.0.1:3000/
+app.get('/', (req, res) => res.send('Hello nodejs'));
+app.listen(port, () => console.log('example app listening on port 3000'))
