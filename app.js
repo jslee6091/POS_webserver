6dbfb11ce98987d43241e3a8c8bd8fb0d8674b27
jslee6091 <jslee6091@naver.com>
2021-05-12 18:11:00 +0900

    pos_server

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-const port = process.env.PORT || 3000;
-// http://127.0.0.1:3000/
-app.get('/', (req, res) => res.send('Hello nodejs jaesung'));
-app.get('/welcome', (req, res)=> res.send('hello welcome to the Nodejs service'));
-app.get('/check', (req, res)=> res.send('hahahahahahahahahahahahahahaha'));
-app.listen(port, () => console.log('example app listening on port 3000'))
